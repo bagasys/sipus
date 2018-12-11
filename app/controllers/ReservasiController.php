d94eb728f6896bf204a015efe45a990a38ee7cce
Fandi <fandipranatajaya@gmail.com>
2018-12-11 17:48:21 +0700

    daftar-peminjaman, daftar-reservasi, hapus peminjaman, hapus reservasi

--- app/controllers/ReservasiController.php
+++ app/controllers/ReservasiController.php
@@ -5,7 +5,22 @@ class ReservasiController extends BaseController
 
 	public function manageAction()
     {
+        if($this->session->get('auth')['status'] != '1'){
+            $this->response->redirect();
+        }
+
+        $id_buku = $this->request->getPost('id_buku');
+        $id_buku= '%'.$id_buku.'%';
+        $reserve = Reservasi::find(
+            [
+                'conditions' => "id_buku LIKE :id_buku:" ,
+                'bind'       => [
+                    'id_buku' => $id_buku,
+                ]
+            ]
+        );
         
+        $this->view->reserve = $reserve;
     }
 
     public function createAction()
@@ -15,7 +30,23 @@ class ReservasiController extends BaseController
 
     public function destroyAction()
     {
+        $id = $this->request->getPost('id');
         
+        $reservasi = Reservasi::findFirst("id = '$id'");
+
+        if ($reservasi !== false) {
+            if ($reservasi->delete() === false) {
+                echo "Sorry, we can't delete the reservation right now: \n";
+        
+                $messages = $reservasi->getMessages();
+        
+                foreach ($messages as $message) {
+                    echo $message, "\n";
+                }
+            } else {
+                $this->response->redirect('daftar-reservasi');
+            }
+        }
     }
 
     public function showAction()
