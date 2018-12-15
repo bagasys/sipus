3f012f2215dc6020d379f4861d800cba45705dcc
Fandi <fandipranatajaya@gmail.com>
2018-12-15 21:13:05 +0700

    coba

--- app/controllers/PeminjamanController.php
+++ app/controllers/PeminjamanController.php
@@ -13,7 +13,7 @@ class PeminjamanController extends BaseController
         //WHERE u.id = p.id_user');
         //$users  = $query->execute();
 
-        $query = $this->modelsManager->createQuery('SELECT p.id, p.id_user, u.id as wkwk, u.nama, p.id_buku, b.judul, p.status, p.tgl_hrs_kembali, p.denda, p.tgl_pinjam FROM Users u, Peminjaman p, Buku b
+        $query = $this->modelsManager->createQuery('SELECT p.id, p.id_user, u.id, u.nama, p.id_buku, b.judul, p.status, p.tgl_hrs_kembali, p.denda, p.tgl_pinjam FROM Users u, Peminjaman p, Buku b
         WHERE u.id = p.id_user AND p.id_buku = b.id AND p.id = 1');
         $peminjamans  = $query->execute();
         
@@ -137,9 +137,9 @@ class PeminjamanController extends BaseController
         // $users = $query->execute();
         
         //$query = $this->modelsManager->createQuery('SELECT u.nama,  FROM Users');
-        $query = $this->modelsManager->createQuery('SELECT u.nama, p.id_buku FROM Users u, Peminjaman p
-        WHERE u.id = p.id_user');
-        $users  = $query->execute();
+        // $query = $this->modelsManager->createQuery('SELECT u.nama, p.id_buku FROM Users u, Peminjaman p
+        // WHERE u.id = p.id_user');
+        // $users  = $query->execute();
         
         // // With bound parameters
         // $query = $this->modelsManager->createQuery('SELECT * FROM Cars WHERE name = :name:');
@@ -149,8 +149,47 @@ class PeminjamanController extends BaseController
         //     ]
         // );
 
-         $this->view->users = $users;
+        $date = date('Y-m-d');
+        $query = $this->modelsManager->createQuery('SELECT id,tgl_hrs_kembali,status FROM Peminjaman WHERE tgl_hrs_kembali < :tgl:');
+        $users = $query->execute(
+            [
+                'tgl' => $date,
+            ]
+        );
+        // foreach($peminjaman as $data){
+        //     echo $data->tgl_hrs_kembali;
+        // }
+        // foreach($peminjaman as $user){
+        //     //$this->view->users = $peminjaman[$i];
+        //     // echo $peminjaman[$i]->tgl_hrs_kembali;
+        //     if($date < $user->tgl_hrs_kembali){
+        //         $this->view->users = $user;
+        //     }
+        foreach($users as $user){
+            $tanggal = $user->tgl_hrs_kembali;
+            $interval = (strtotime($date) - strtotime($tanggal)) / 86400;
+            $denda = $interval * 5000;
+            echo $interval;
+            echo $user->status;
+            if($denda > 0){
+                $sql = $this->modelsManager->createQuery('UPDATE Peminjaman SET denda = :denda:, status = :stat: WHERE id = :id:');
+                $update = $sql->execute(
+                    [
+                        'denda' => $denda,
+                        'id' => $user->id,
+                        'stat' => 'telat',
+                    ]
+                );  
+            }
+        }
 
+       // }   
+        // if($date > $peminjaman[$i]){
+
+        //    // $this->view->users = $peminjaman[0];
+        // }
+
+         
 
 
 
