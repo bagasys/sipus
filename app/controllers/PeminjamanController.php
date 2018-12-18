7fbdc6f117ea1efc39be63da3be7d8953e6fb014
Fandi <fandipranatajaya@gmail.com>
2018-12-18 12:00:59 +0700

    search peminjaman by judul buku

--- app/controllers/PeminjamanController.php
+++ app/controllers/PeminjamanController.php
@@ -31,6 +31,13 @@ class PeminjamanController extends BaseController
             $peminjamans  = $query->execute([
                 'searchKey' => $searchKey,
             ]);
+        } else if ($searchBy == 'judul'){
+            $searchKey = '%'.$searchKey.'%';
+            $query = $this->modelsManager->createQuery('SELECT p.id as idp, p.id_user, u.id, u.nama, p.id_buku, b.judul, p.status, p.tgl_hrs_kembali, p.denda, p.tgl_pinjam FROM Users u, Peminjaman p, Buku b
+            WHERE u.id = p.id_user AND p.id_buku = b.id AND b.judul LIKE :searchKey:');
+            $peminjamans  = $query->execute([
+                'searchKey' => $searchKey,
+            ]);
         } else {
             $query = $this->modelsManager->createQuery('SELECT p.id as idp, p.id_user, u.id, u.nama, p.id_buku, b.judul, p.status, p.tgl_hrs_kembali, p.denda, p.tgl_pinjam FROM Users u, Peminjaman p, Buku b
             WHERE u.id = p.id_user AND p.id_buku = b.id AND p.id_buku = :searchKey:');
